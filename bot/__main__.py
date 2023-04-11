2a3547a102397d73814b027fa160d5bdcc6602f4
Fobos <95380054+Yastrah@users.noreply.github.com>
2023-04-11 16:41:28 +0300

    Testing file scope

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,10 +1,10 @@
 import logging
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher, executor, types
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -17,10 +17,12 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
+    with open("../data/file.txt", 'r') as file:
+        data = file.read().strip()
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply(f"Hi!\nI'm EchoBot!\n{data}.")
 
 
 @dp.message_handler(regexp='(^cat[s]?$|puss)')
